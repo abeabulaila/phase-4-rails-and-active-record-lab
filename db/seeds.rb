37a5a3974fd10bcca53798bce309167c6c69c27f
Ibrahim Abulaila <abeabulaila@gmail.com>
2022-01-10 13:37:26 -0500

    commit tree 7a2989e26e71501d22791021130e71bbd1d73b8b

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Student.create!(first_name: 'Cheddar', last_name: 'Bob', grade: 5)
+Student.create!(first_name: 'Easy', last_name: 'Styles', grade: 4)
+Student.create!(first_name: 'Tony', last_name: 'Montana', grade: 511)
+
